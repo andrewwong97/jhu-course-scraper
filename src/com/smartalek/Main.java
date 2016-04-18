a098329eb74ede35ec51a9a492ebc707f0dec8e0
andrewwong97 <aznhippos@gmail.com>
2016-04-18 02:34:19 -0400

    prints prereqs to standard output

--- src/com/smartalek/Main.java
+++ src/com/smartalek/Main.java
@@ -1,14 +1,15 @@
 import com.jaunt.*;
 import com.jaunt.component.*;
 import java.io.*;
+import java.lang.String;
 
 
 public class Main{
     public static void main(String[] args) throws IOException{
         try{
-            FileWriter outFile = new FileWriter("output.txt");
+            // FileWriter outFile = new FileWriter("output.txt");
 
-            String courseName = "EN.600.226";
+            String courseName = "EN.600.463"; // placeholder Course
             UserAgent userAgent = new UserAgent();
             userAgent.visit("https://isis.jhu.edu/classes/");
             Document doc = userAgent.doc;
@@ -24,15 +25,42 @@ public class Main{
 
             doc = userAgent.doc; // 2nd page
             form = doc.getForm(0);
-            Elements tables = doc.findEach("<table>");       //find non-nested tables
-            for(Element table : tables) {
-                System.out.println(table.outerHTML() + "\n----\n");      //print each element and its contents
-                outFile.write(table.outerHTML() + "\n----\n");
+            Elements odd_trs = doc.findEach("<table>").findEach("<td>").findEach("<tr class=odd>");
+            Elements even_trs = doc.findEach("<table>").findEach("<td>").findEach("<tr class=even>");
+
+            for (item : odd_trs) {
+                getPrereqs(getID(odd_trs));
             }
-            outFile.close();
+
+
         }
         catch(JauntException e){
             System.out.println(e);
         }
+    }
+
+    private static String getID(Elements list) {
+        String s = "";
+        for (Element td : list) {
+            String idBlock = null;
+            try {
+                idBlock = td.findFirst("<td class=icon-none-16-right>").innerHTML();
+            } catch (NotFound notFound) {
+                notFound.printStackTrace();
+            }
+            int place = idBlock.indexOf("blah_");
+            return idBlock.substring(place+5,place+11);
+        }
+        return s;
+    }
+
+    private static void getPrereqs(String id) throws ResponseException, NotFound {
+        UserAgent u = new UserAgent();
+        String url = "https://isis.jhu.edu/classes/ClassDetails.aspx?id=" + id;
+        u.visit(url);
+        Document doc = u.doc;
+        String courseDesc = doc.findFirst("<span id=sectionDetails_lblDescription").innerHTML();
+        int start = courseDesc.indexOf("Recommended Course Background: ");
+        System.out.println(courseDesc.substring(start+31));
     }
 }
